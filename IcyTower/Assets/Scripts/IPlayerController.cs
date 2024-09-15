272aba39d14b268eeae73f4da12d277f23e7bff0
mosmatan <matanmo@mta.ac.il>
2024-09-15 21:22:19 +0300

    IPlayerController interface

---
+++ IcyTower/Assets/Scripts/IPlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public interface IPlayerController
+    {
+        KeyCode RightKey { get; set; }
+        KeyCode LeftKey { get; set; }
+        KeyCode JumpKey { get; set; }
+
+    }
+}
--- IcyTower/Assets/Scripts/PlayerController.cs
+++ IcyTower/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,19 @@
 using Assets.Scripts;
 using UnityEngine;
 
-public class PlayerController : Singleton<PlayerController>
+public class PlayerController : MonoBehaviour, IPlayerController
 {
     private IJumper jumper;
+
+    public KeyCode RightKey { get; set; } = KeyCode.D;
+    public KeyCode LeftKey { get; set; } = KeyCode.A;
+    public KeyCode JumpKey { get; set; } = KeyCode.Space;
 
     // Start is called before the first frame update
     void Start()
     {
         jumper = GameObject.FindAnyObjectByType<Jumper>();
+        Debug.Log(RightKey.ToString()+ LeftKey.ToString()+ JumpKey.ToString());
     }
 
     // Update is called once per frame
@@ -19,11 +24,20 @@ public class PlayerController : Singleton<PlayerController>
 
     private void move()
     {
-        int xAxis = (int)Input.GetAxisRaw("Horizontal");
+        int xAxis = 0;
+
+        if (Input.GetKey(RightKey))
+        {
+            xAxis = 1;
+        }
+        else if (Input.GetKey(LeftKey)) 
+        {
+            xAxis = -1;
+        }
 
         jumper.Move(xAxis);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(JumpKey))
         {
             jumper.Jump();
         }
