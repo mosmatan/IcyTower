c63e485407d431da2eea4048f38ae6c687f907c3
danareshef1 <sbv2605@gmail.com>
2024-10-12 16:23:12 +0300

    last changes

--- IcyTower/Assets/Scripts/Bricks/Brick.cs
+++ IcyTower/Assets/Scripts/Bricks/Brick.cs
@@ -100,7 +100,7 @@ public class Brick : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.transform.CompareTag("Player"))
+        if (other.transform.CompareTag("Player") && !isFalling)
         {
             handleShake(); // Trigger shake effect on player collision.
         }
@@ -139,6 +139,6 @@ public class Brick : MonoBehaviour
         }
 
         isFalling = false; 
-        isShaking = false; 
+        isShaking = false;
     }
 }
